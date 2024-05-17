e64610740fa4ff681d8d0ed2546190f1a4968ebc
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-18 02:30:00 +0500

    1 or 3 display O if  2 or 4 E

--- L6p1.asm
+++ L6p1.asm
@@ -1,3 +1,4 @@
+;if 1 or 3, display "O"; if  2 or 4, display "E",
 dosseg
  .model small
  .stack 100h
@@ -5,34 +6,44 @@ dosseg
   .code
     main proc
 
-    mov ah,8
+    mov ah,1  ; get input
     int 21h
+    
+
     mov dl,al
     
     CMP	dl,'1'
     je displayO
+    
     CMP dl,'3'
     je displayO
 
     CMP dl,'2'
     je displayE
+
     CMP dl,'4'
     je displayE
+    
+      jmp endProgram
 
     displayO:
+  
     mov dl,'O'
     mov ah,2
     int 21h
-      mov ah, 4ch
-     int 21h
- 
+
+      jmp endProgram
+
     displayE:
+  
     mov dl,'E'
     mov ah,2
     int 21h
     
-
-     mov ah, 4ch
-     int 21h
+    endProgram:
+    
+    mov ah, 4ch
+    int 21h
+    
     main endp
   end main
