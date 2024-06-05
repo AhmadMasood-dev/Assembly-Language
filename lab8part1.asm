f8fe92c2ed1c72600295546b6bde403907002f9b
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-06-05 23:58:43 +0500

    print 3 arrays using call function

--- lab8part1.asm
+++ lab8part1.asm
@@ -1,35 +1,45 @@
-
-dosseg
 .model small
 .stack 100h
 .data
-  var1 db 'Ahmad$'
-  var2 db 'Masood$'
-  var3 db 'Gondal$'
+  arr1 db 'A','h','m','a','d','$'
+  arr2 db 'M','a','s','o','o','d','$'
+  arr3 db 'G','o','n','d','a','l','$'
+
 .code
 main proc
-           mov  ax, @data
-           mov  ds, ax
+           mov  ax,@data
+           mov  ds,ax
 
-           lea  dx, var1
+           lea  si,arr1
            call _PRT_STR
 
-           lea  dx, var2
+           lea  si,arr2
            call _PRT_STR
 
-           lea  dx, var3
+           lea  si,arr3
            call _PRT_STR
-
-           mov  ah, 4ch
+           
+           mov  ah,4ch
            int  21h
+main endp
 
 _PRT_STR proc
+           mov  ah,2
+  loop1:   
+           mov  dl,[si]
+           int  21h
 
-           mov  ah, 9
+           inc  si
+
+           mov  dl,[si]
+           cmp  dl,'$'
+
+           jne  loop1
+
+           mov  dl,10
            int  21h
 
            ret
 _PRT_STR endp
 
-main endp
 end main
