4e42f20410291a0704ccb3e63e0b825f996f42d1
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-06-15 00:27:50 +0500

    optimize the code

--- part3Task2.asm
+++ part3Task2.asm
@@ -9,55 +9,50 @@ dosseg
  .model small
   .stack 100h
 .data
-    var1 db 'ENETR A NUMBER: $'
+  var1 db 'ENETR A NUMBER: $'
 .code
 main proc
    
-              mov ax,@data
-              mov ds,ax
+            mov  ax,@data
+            mov  ds,ax
 
-              lea dx,var1      ; show msg
-              mov ah,9
-              int 21h
+            lea  dx,var1    ; show msg
+            mov  ah,9
+            int  21h
 
-              mov ah,1         ;take number
-              int 21h
-              sub al,48        ;for ascii
+            mov  ah,1       ;take number
+            int  21h
+            sub  al,48      ;for ascii
 
-              mov ch,al        ;move to ch for outer loop
+            mov  cl,al      ;move to ch for outer loop
 
-              mov dl,10        ;next line
-              mov ah,2
-              int 21h
+            mov  dl,10      ;next line
+            mov  ah,2
+            int  21h
 
-    upperloop:
+  upperloop:
 
    
-              mov cl,1         ;innerloop iteration
+            mov  bl,cl      ;innerloop iteration
    
-              mov dl,42        ;ascii of *
+            mov  dl,42      ;ascii of *
               
-    innerloop:
+  innerloop:
 
-              mov ah,2
-              int 21h
+            mov  ah,2
+            int  21h
 
-              inc cl
- 
-              cmp cl,ch
-              jle innerloop
+            LOOP innerloop
 
-              mov dl,10        ;for next line
-              mov ah,2
-              int 21h
+            mov  dl,10      ;for next line
+            mov  ah,2
+            int  21h
+            mov  cl,bl
 
-
-              dec ch
-              jnz upperloop
+            LOOP upperloop
     
-      
-              mov ah,4ch
-              int 21h
+            mov  ah,4ch
+            int  21h
     
     
 main endp
